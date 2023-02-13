fe352cd20e0a1b4753d855f5bf860a14ea2e1ba5
Thanwa Chaleonyos <aunkerzerza@hotmail.com>
2023-02-13 22:00:21 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './views/pages/Home'
+function App() {
+  return (
+    <div style={{background:"red",width:"100vw",hight:"100vh"}} >
+      d
+         
+    
+    </div>
+  );
+}
+
+export default App;
